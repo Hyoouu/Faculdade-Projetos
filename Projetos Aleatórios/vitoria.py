60addbba4324ac77052e27851e9c06f9c0af9326
Gustavo <gustaf12445@gmail.com>
2025-10-02 23:44:08 -0300

    ignorar

--- Projetos Aleatórios/vitoria.py
+++
@@ -1,9 +0,0 @@
-errado = "Impostora!"
-
-while (True) :
-    nome = input(("Digite seu nome: ")).strip().lower()
-    if nome  == "vitoria" : 
-        print("Gustavo te ama meu bem S2")
-        break
-    else : 
-        print(errado) 
