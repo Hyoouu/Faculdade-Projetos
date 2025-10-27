d741fcf65b301d8b9e556ecd88840936c8f20142
Gustavo <gustaf12445@gmail.com>
2025-10-27 17:51:52 -0300

    Jogo da Velha Prototipo2

--- Projetos Aleatórios/Jogo da Velha.py
+++ Projetos Aleatórios/Jogo da Velha.py
@@ -1,13 +1,88 @@
 import random
 
 matriz = [['' for _ in range(3)] for _ in range(3)]
-for linha in matriz: 
-    print(linha)
-
 contador = 0
 inicio = random.randint(0, 1)
+modo = int(input("--------------------------\n1 - Jogador vs Computador\n2 - Jogador vs Jogador\nEscolha o modo de jogo: "))
 
-while True: 
+print("----------------\nInício do Jogo!\n----------------")
+for linha in matriz:
+        print(linha)
+
+while True:
+
+  # ...código do modo Jogador vs Computador...
+ if modo == 1:
+
+    if inicio == 0:
+     print("----------------\nSua vez de jogar!\n----------------")
+     while True:
+      try:
+       linha = int(input("Digite a linha(de 0 a 2): "))
+       coluna = int(input("Digite a coluna(de 0 a 2): "))
+      except ValueError:
+         print("Valor inválido, tente novamente.")
+         continue
+      if linha <0 or linha >2 or coluna <0 or coluna >2 :
+          print("Valor inválido, tente novamente.")
+          continue
+      if  matriz[linha][coluna] == "X" or matriz[linha][coluna] == "O": 
+          print("----------------\nPosição já ocupada, tente novamente.")
+      elif matriz[linha][coluna] == '':
+           matriz[linha][coluna] = "X" 
+           print("----------------")
+           for linha in matriz:
+              print(linha)
+           break
+     if matriz[0][0] == "X" and matriz[0][1] == "X" and matriz[0][2] == "X" or\
+        matriz[1][0] == "X" and matriz[1][1] == "X" and matriz[1][2] == "X" or\
+        matriz[2][0] == "X" and matriz[2][1] == "X" and matriz[2][2] == "X" or\
+        matriz[0][0] == "X" and matriz[1][0] == "X" and matriz[2][0] == "X" or\
+        matriz[0][1] == "X" and matriz[1][1] == "X" and matriz[2][1] == "X" or\
+        matriz[0][2] == "X" and matriz[1][2] == "X" and matriz[2][2] == "X" or\
+        matriz[0][0] == "X" and matriz[1][1] == "X" and matriz[2][2] == "X" or\
+        matriz[0][2] == "X" and matriz[1][1] == "X" and matriz[2][0] == "X":
+        print(f"Você venceu em {contador} rodadas!")
+        break
+     elif all(matriz[linha][coluna] != '' for linha in range(3) for coluna in range(3)):
+         print("Deu Velha!")
+         break
+     inicio = 1 
+        
+    elif inicio == 1:  
+     print("----------------\nVez do Computador!\n----------------")
+     
+     while True:
+      linha2 = random.randint(0, 2)
+      coluna2 = random.randint(0, 2)
+      if matriz[linha2][coluna2] == "X" or matriz[linha2][coluna2] == "O": 
+          print("----------------\nPosição já ocupada, tente novamente.")
+          continue
+      elif matriz[linha2][coluna2] == '':
+          matriz[linha2][coluna2] = "O"
+          for linha in matriz:
+              print(linha)
+          break
+
+     if   matriz[0][0] == "O" and matriz[0][1] == "O" and matriz[0][2] == "O" or\
+          matriz[1][0] == "O" and matriz[1][1] == "O" and matriz[1][2] == "O" or\
+          matriz[2][0] == "O" and matriz[2][1] == "O" and matriz[2][2] == "O" or\
+          matriz[0][0] == "O" and matriz[1][0] == "O" and matriz[2][0] == "O" or\
+          matriz[0][1] == "O" and matriz[1][1] == "O" and matriz[2][1] == "O" or\
+          matriz[0][2] == "O" and matriz[1][2] == "O" and matriz[2][2] == "O" or\
+          matriz[0][0] == "O" and matriz[1][1] == "O" and matriz[2][2] == "O" or\
+          matriz[0][2] == "O" and matriz[1][1] == "O" and matriz[2][0] == "O":
+          print(f"Você perdeu em {contador} rodadas!")
+          break
+     elif all(matriz[linha][coluna] != '' for linha in range(3) for coluna in range(3)):
+         print("Deu Velha!")
+         break
+     inicio = 0
+    
+         
+    # ...código do modo Jogador vs Jogador...   
+ elif modo == 2:
+    pass
 
     if inicio == 0:
      
@@ -43,20 +118,26 @@ while True:
          print("Deu Velha!")
          break
      inicio = 1
-
-    elif inicio == 1:
      
+    elif inicio == 1:
+        
      while True:
-      linha2 = random.randint(0, 2)
-      coluna2 = random.randint(0, 2)
-      if matriz[linha2][coluna2] == "X" or matriz[linha2][coluna2] == "O": 
-          print("Posição já ocupada, tente novamente.")
+      try:
+       linha2 = int(input("Digite a linha(de 0 a 2): "))
+       coluna2 = int(input("Digite a coluna(de 0 a 2): "))
+      except ValueError:
+         print("Valor inválido, tente novamente.")
+         continue
+      if linha2 <0 or linha2 >2 or coluna2 <0 or coluna2 >2 :
+          print("Valor inválido, tente novamente.")
           continue
+      if  matriz[linha2][coluna2] == "X" or matriz[linha2][coluna2] == "O": 
+          print("Posição já ocupada, tente novamente.")
       elif matriz[linha2][coluna2] == '':
-          matriz[linha2][coluna2] = "O"
-          for linha in matriz:
+           matriz[linha2][coluna2] = "O" 
+           for linha in matriz:
               print(linha)
-          break
+           break
 
      if   matriz[0][0] == "O" and matriz[0][1] == "O" and matriz[0][2] == "O" or\
           matriz[1][0] == "O" and matriz[1][1] == "O" and matriz[1][2] == "O" or\
@@ -76,4 +157,4 @@ while True:
      if contador < 9:
         contador += 1
         print(f"turno {contador}")
-     
+              
